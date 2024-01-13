3db9f0f56fba51307603cbabef2cf9f6f4cabeb7
loadrp <lucas.rodrigues.prado120@gmail.com>
2024-01-13 18:33:57 -0300

    add sonner toasts

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { RouterProvider } from "react-router-dom";
 import { router } from './routes'
 import "./global.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from "sonner";
 
 export function App() {
   return (  
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router}/>
+      <Toaster richColors closeButton containerAriaLabel="Notificação"/>
     </HelmetProvider>
      
   );
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,46 +2,64 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import { Helmet } from "react-helmet-async";
-import { useForm } from 'react-hook-form'
-import {z} from 'zod'
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from "sonner";
 
 const signInForm = z.object({
-    email: z.string().email(),
-})
+  email: z.string().email(),
+});
 
-type SignInForm = z.infer<typeof signInForm>
+type SignInForm = z.infer<typeof signInForm>;
 
-export function SignIn(){
-    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>();
+export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
 
-    async function handleSignIn(data: SignInForm) {
-        console.log(data)
-        await new Promise(resolve => setTimeout(resolve,2000))
-
+  async function handleSignIn(data: SignInForm) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success("Enviamos um link de autenticacao para seu e-mail.",{
+        description: 'Fique atento ao seu e-mail',
+        action: {
+            label: "Reenviar",
+            onClick: () => handleSignIn(data)
+        }
+      });
+    } catch {
+        toast.error("Credenciais Inválidas")
     }
-    
-    return(
-        <>
-        <Helmet title="Login"/>
-        <div className="p-8">
-            <div className="w-[340px] flex flex-col justify-center gap-6">
-                <div className="flex flex-col gap-2 text-center">
-                <h1 className="text-2xl font-semibold tracking-tighter">Acessar Painel</h1>
-                <p className="text-sm text-muted-foreground">
-                    Acompanhe suas vendas pelo painel do parceiro
-                </p>
-                <form onSubmit={handleSubmit(handleSignIn)}className="space-y-4">
-                    <div className="space-y-2">
-                        <Label className="flex font-semibold" htmlFor="email">Seu e-mail</Label>
-                        <Input id="email" type="email" {...register('email')}></Input>
-                    </div>
-                    <Button disabled={isSubmitting} className="w-full" type="submit">Acessar Painel</Button>
+  }
 
-                </form>
-                </div>
-
-            </div>
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="p-8">
+        <div className="flex w-[340px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tighter">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+            <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+              <div className="space-y-2">
+                <Label className="flex font-semibold" htmlFor="email">
+                  Seu e-mail
+                </Label>
+                <Input id="email" type="email" {...register("email")}></Input>
+              </div>
+              <Button disabled={isSubmitting} className="w-full" type="submit">
+                Acessar Painel
+              </Button>
+            </form>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
