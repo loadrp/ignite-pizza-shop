64c2c9c5ac56d70d2dd92fa9f494d6003e3d6e67
loadrp <lucas.rodrigues.prado120@gmail.com>
2024-01-19 09:30:02 -0300

    add products chart

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -5,6 +5,8 @@ import { MonthRevenueCard } from "./month-revenue-card";
 import { MonthOrdersAmountCard } from "./month-orders-amount-card";
 import { DayOrdersAmountCard } from "./day-orders-amount";
 import { MonthCanceledOrdersAmountCard } from "./month-canceled-orders-amount";
+import { RevenueChart } from "./revenue-chart";
+import { PopularProductsChart } from "./popular-products-chart";
 
 export function Dashboard() {
   return (
@@ -19,6 +21,11 @@ export function Dashboard() {
             <DayOrdersAmountCard/>
             <MonthCanceledOrdersAmountCard/>
           
+        </div>
+        <div className="grid grid-cols-9 gap-4" >
+          <RevenueChart/>
+          <PopularProductsChart/>
+
         </div>
       </div>
     </>
