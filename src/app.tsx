6ef3deaea4900fc028743e9845b89cef31afe0bd
loadrp <lucas.rodrigues.prado120@gmail.com>
2024-01-20 11:10:13 -0300

    add axios and react query

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,22 @@
 import { RouterProvider } from "react-router-dom";
-import { router } from './routes'
+import { router } from "./routes";
 import "./global.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
-  return (  
+  return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
       <ThemeProvider defaultTheme="dark" storageKey="pizza-shop-theme">
-      <RouterProvider router={router}/>
-      <Toaster richColors containerAriaLabel="Notificação"/>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+        <Toaster richColors containerAriaLabel="Notificação" />
       </ThemeProvider>
-     
     </HelmetProvider>
-     
   );
 }
