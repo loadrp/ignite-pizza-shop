8aceeee4dd6a2b0c994586dcbc90e09cbc554db6
loadrp <lucas.rodrigues.prado120@gmail.com>
2024-02-18 11:35:18 -0300

    make filters functional

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,27 +1,38 @@
-import { api } from "@/lib/axios"
+import { api } from "@/lib/axios";
 
-export interface GetOrdersQuery{
-  pageIndex?: number | null
+export interface GetOrdersQuery {
+  pageIndex?: number | null;
+  orderId?: string | null;
+  customerName?: string | null;
+  status?: string | null;
 }
 export interface GetOrdersResponse {
   orders: {
-    orderId: string
-    total: number
-    status: 'pending' | 'processing' |'delivering' | 'delivered' | 'canceled'
-    createdAt: string
-    customerName: string
-  }[]
+    orderId: string;
+    total: number;
+    status: "pending" | "processing" | "delivering" | "delivered" | "canceled";
+    createdAt: string;
+    customerName: string;
+  }[];
   meta: {
-    pageIndex: number
-    totalCount: number
-    perPage: number
-  }
+    pageIndex: number;
+    totalCount: number;
+    perPage: number;
+  };
 }
-export async function getOrders({pageIndex}:GetOrdersQuery) {
-  const response = await api.get<GetOrdersResponse>('/orders', {
+export async function getOrders({
+  pageIndex,
+  customerName,
+  orderId,
+  status,
+}: GetOrdersQuery) {
+  const response = await api.get<GetOrdersResponse>("/orders", {
     params: {
       pageIndex,
+      customerName,
+      orderId,
+      status,
     },
-  })
-  return response.data
+  });
+  return response.data;
 }
